d42b90823e9c387b7aa1b00335a9ef1492e2c848
Zhenguo <jingle1267@163.com>
2014-12-17 23:52:28 +0800

    [NewFeature] Add bounder scroll.

--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -21,37 +21,39 @@ public class MainActivity extends Activity {
     private List<ImageView> imageViewList;
     private LinearLayout indicator;
 
+    private int[] ids = {R.drawable.banner1, R.drawable.banner2, R.drawable.banner3, R.drawable.banner4};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.auto_scroll_view_pager_demo);
+        init();
+        Log.d("test", "radio button count : " + indicator.getChildCount());
+    }
 
-        viewPager = (AutoScrollViewPager) findViewById(R.id.view_pager);
-
+    private void init() {
         imageViewList = new ArrayList<ImageView>();
 
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        ImageView view1, view2, view3, view4;
-        view1 = new ImageView(this);
-        view2 = new ImageView(this);
-        view3 = new ImageView(this);
-        view4 = new ImageView(this);
-        view1.setLayoutParams(params);
-        view2.setLayoutParams(params);
-        view3.setLayoutParams(params);
-        view4.setLayoutParams(params);
-        view1.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        view2.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        view3.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        view4.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        view1.setImageResource(R.drawable.banner1);
-        view2.setImageResource(R.drawable.banner2);
-        view3.setImageResource(R.drawable.banner3);
-        view4.setImageResource(R.drawable.banner4);
-        imageViewList.add(view1);
-        imageViewList.add(view2);
-        imageViewList.add(view3);
-        imageViewList.add(view4);
+        viewPager = (AutoScrollViewPager) findViewById(R.id.view_pager);
+        indicator = (LinearLayout) findViewById(R.id.indicator);
+
+        for (int i = 0; i < ids.length; i++) {
+            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            ImageView view = new ImageView(this);
+            view.setLayoutParams(params);
+            view.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            view.setImageResource(ids[i]);
+            imageViewList.add(view);
+
+            View v = new View(this);
+            v.setBackgroundResource(R.drawable.banner_pagecontrol_normal);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(12, 12);
+            layoutParams.setMargins(4, 4, 4, 4);
+            if (i == 0) {
+                v.setBackgroundResource(R.drawable.banner_pagecontrol_selected);
+            }
+            indicator.addView(v, layoutParams);
+        }
 
         viewPager.setAdapter(pagerAdapter);
         viewPager.setOnPageChangeListener(new MyOnPageChangeListener());
@@ -59,39 +61,20 @@ public class MainActivity extends Activity {
         viewPager.setInterval(1000);
         viewPager.startAutoScroll();
         viewPager.setCycle(true);
-        // viewPager.setCurrentItem(Integer.MAX_VALUE / 2 - Integer.MAX_VALUE / 2 % imageViewList.size());
-        viewPager.setCurrentItem(0);
-
-        View v1 = new View(this);
-        View v2 = new View(this);
-        View v3 = new View(this);
-        View v4 = new View(this);
-        v1.setBackgroundResource(R.drawable.banner_pagecontrol_normal);
-        v2.setBackgroundResource(R.drawable.banner_pagecontrol_normal);
-        v3.setBackgroundResource(R.drawable.banner_pagecontrol_normal);
-        v4.setBackgroundResource(R.drawable.banner_pagecontrol_normal);
-
-        indicator = (LinearLayout) findViewById(R.id.indicator);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(12, 12);
-        layoutParams.setMargins(4, 4, 4, 4);
-        indicator.addView(v1, 0, layoutParams);
-        indicator.addView(v2, 1, layoutParams);
-        indicator.addView(v3, 2, layoutParams);
-        indicator.addView(v4, 3, layoutParams);
-
-        Log.d("test", "radio button count : " + indicator.getChildCount());
+        int pos = Integer.MAX_VALUE / 2 - Integer.MAX_VALUE / 2 % imageViewList.size();
+        viewPager.setCurrentItem(pos);
     }
 
     public class MyOnPageChangeListener implements OnPageChangeListener {
 
         @Override
         public void onPageSelected(int position) {
-            Log.d("test", "" + indicator.getChildAt(position));
+            int pos = position % imageViewList.size();
+            Log.d("test", "" + indicator.getChildAt(pos));
             for (int i = 0; i < indicator.getChildCount(); i++) {
                 indicator.getChildAt(i).setBackgroundResource(R.drawable.banner_pagecontrol_normal);
             }
-            indicator.getChildAt(position).setBackgroundResource(R.drawable.banner_pagecontrol_selected);
-            // Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            indicator.getChildAt(pos).setBackgroundResource(R.drawable.banner_pagecontrol_selected);
         }
 
         @Override
@@ -125,14 +108,13 @@ public class MainActivity extends Activity {
 
         @Override
         public int getCount() {
-
-            return imageViewList.size();
+            return Integer.MAX_VALUE;
         }
 
         @Override
         public void destroyItem(ViewGroup container, int position,
                                 Object object) {
-            container.removeView(imageViewList.get(position));
+            container.removeView(imageViewList.get(position % imageViewList.size()));
 
         }
 
@@ -150,14 +132,14 @@ public class MainActivity extends Activity {
 
         @Override
         public Object instantiateItem(ViewGroup container, final int position) {
-            container.addView(imageViewList.get(position));
-            imageViewList.get(position).setOnClickListener(new View.OnClickListener() {
+            container.addView(imageViewList.get(position % imageViewList.size()), 0);
+            imageViewList.get(position % imageViewList.size()).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(MainActivity.this, "click position : " + position, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "click position : " + position % imageViewList.size(), Toast.LENGTH_SHORT).show();
                 }
             });
-            return imageViewList.get(position);
+            return imageViewList.get(position % imageViewList.size());
         }
 
     };
